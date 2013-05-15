9e9b33da100e7e196ac4890d01cce37fc2ab9649
Vijay Oommen <vioommen@Vijays-MacBook-Pro.local>
2013-05-15 15:14:09 -0500

    Initial Commit

---
+++ MHHS-DR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MHHS-DR
+//
+//  Created by Vijay Oommen on 5/15/13.
+//  Copyright (c) 2013 MHHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MHHSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MHHSAppDelegate class]));
+    }
+}
